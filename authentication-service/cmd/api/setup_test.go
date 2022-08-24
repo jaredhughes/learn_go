d94d8824627045cc3137567af6003dfdd405d0eb
Jared Hughes <j@redhugh.es>
2022-08-24 20:47:47 +0700

    Add auth tests with repository interface

--- authentication-service/cmd/api/setup_test.go
+++ authentication-service/cmd/api/setup_test.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"authentication/cmd/data"
 	"os"
 	"testing"
 )
 
+var testApp Config
+
 func TestMain(m *testing.M) {
+	repo := data.NewPostgresTestRepository(nil)
+	testApp.Repo = repo
+
 	os.Exit(m.Run())
 }
