494c16ef361f124b9901318581a67e1ad2bba2ef
Jared Hughes <j@redhugh.es>
2022-08-24 20:46:22 +0700

    Add logging invocation to auth service

--- authentication-service/cmd/api/handlers.go
+++ authentication-service/cmd/api/handlers.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"net/http"
@@ -24,13 +26,19 @@ func (app *Config) Authenticate(w http.ResponseWriter, r *http.Request) {
 	// validate user in DB
 	user, err := app.Models.User.GetByEmail(requestPayload.Email)
 	if err != nil {
-		tools.ErrorJSON(w, errors.New("Invalid credentials"), http.StatusBadRequest)
+		tools.ErrorJSON(w, errors.New("invalid credentials"), http.StatusBadRequest)
 		return
 	}
 
-	valid, err := user.PasswordMatches(requestPayload.Password)
+	valid, err := app.Models.User.PasswordMatches(requestPayload.Password)
 	if err != nil || !valid {
-		tools.ErrorJSON(w, errors.New("Invalid credentials"), http.StatusBadRequest)
+		tools.ErrorJSON(w, errors.New("invalid credentials"), http.StatusBadRequest)
+		return
+	}
+
+	err = app.logRequest("authentication", fmt.Sprintf("%s logged in", user.Email))
+	if err != nil {
+		app.errorJSON(w, err)
 		return
 	}
 
@@ -41,4 +49,30 @@ func (app *Config) Authenticate(w http.ResponseWriter, r *http.Request) {
 	}
 
 	tools.WriteJSON(w, http.StatusAccepted, payload)
+}
+
+func (app *Config) logRequest(name, data string) error {
+	var entry struct {
+		Name string `json:"name"`
+		Data string `json:"data"`
+	}
+
+	entry.Name = name
+	entry.Data = data
+
+	jsonData, _ := json.MarshalIndent(entry, "", "\t")
+	logServiceUrl := "http://logger-service/log"
+
+	request, err := http.NewRequest("POST", logServiceUrl, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return err
+	}
+
+	client := &http.Client{}
+	_, err = client.Do(request)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
